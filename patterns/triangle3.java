1e531e8fef0bb67cdc70dcbde9ab2b304fbd1b2c
Aditya <aaditya1392@gmail.com>
2022-09-17 16:31:35 +0530

    made a new pattern

--- patterns/triangle3.java
+++ patterns/triangle3.java
@@ -12,7 +12,13 @@ public class triangle3 {
         int a = scan.nextInt();
         scan.close();
         for (int i=1; i<=a; i++){
-            
+            for (int j=1; j<=2*(a-i); j++){
+                System.out.print(" ");
+            }
+            for (int j=1; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
         }
     }
 }
